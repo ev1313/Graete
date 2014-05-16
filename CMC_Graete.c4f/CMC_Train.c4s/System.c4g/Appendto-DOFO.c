05ae1e7bc4cd0662e416fb8897f70b3d662f913b
Tim Blume <tim_blume@web.de>
2014-05-16 09:31:46 +0200

    fixed fog completely now

--- CMC_Graete.c4f/CMC_Train.c4s/System.c4g/Appendto-DOFO.c
+++ CMC_Graete.c4f/CMC_Train.c4s/System.c4g/Appendto-DOFO.c
@@ -4,7 +4,20 @@
 
 #appendto DOFO
 
-//hier gibt es keinen zuf√§lligen Wind 
+//hier gibt es keinen zuf‰lligen Wind 
+
+protected func Timer()
+{
+  //Effektmanager beachten
+  if(!GetEffectData(EFSM_Fog)) return;
+
+  //Nebel erstellen
+  DoFog();
+
+  //Bewegung des Windes
+  if(!GetEffect("IntPush",this))
+    AddEffect("IntPush", this, 50, 1, this, GetID(), 35*(3+Random(10)));
+}
 
 public func FxIntPushTimer(object pTarget, int iEffectNumber, int iEffectTime)
 {
